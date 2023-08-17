cb388f2f07b168dce389f8e9f888fa0b5077d17a
Lex.Margin <1066840101@qq.com>
2023-08-17 19:14:35 +0800

    -- fix   - change vue mode

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import center from './components/center.vue'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'hash',
+    mode: 'history',
     routes: [
         // {
         //   path: '*',
